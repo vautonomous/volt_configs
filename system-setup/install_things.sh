adb58ed0c1837be867812b10c63878566409d530
Volt <volt.autonomous@gmail.com>
2022-06-09 11:18:09 +0300

    update install_things

--- system-setup/install_things.sh
+++ system-setup/install_things.sh
@@ -42,22 +42,44 @@ net-tools \
 sudo -u $USER_NAME mkdir /home/$USER_NAME/programs
 sudo -u $USER_NAME mkdir /home/$USER_NAME/projects
 
+# Dunst
+
+apt install -y libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev libpango1.0-dev libgtk-3-dev libxdg-basedir-dev libnotify-dev
+
+cd /home/$USER_NAME/programs
+sudo -u $USER_NAME git clone https://github.com/dunst-project/dunst.git
+cd dunst
+sudo -u $USER_NAME make
+make install
+cd /home/$USER_NAME/
+sudo -u $USER_NAME rm -rf /home/$USER_NAME/programs/dunst
+
 # i3blocks
 
 cd /home/$USER_NAME/programs
 sudo -u $USER_NAME git clone https://github.com/vivien/i3blocks-contrib.git
-sudo -u $USER_NAME cp /home/$USER_NAME/knowledge-base/pages/system-setup/.i3blocks.conf /home/$USER_NAME/
+sudo -u $USER_NAME cp /home/$USER_NAME/volt_configs/i3-config/.i3blocks.conf /home/$USER_NAME/
 cd /home/$USER_NAME/programs/i3blocks-contrib
 sudo -u $USER_NAME make
 cd /home/$USER_NAME/
 
 # i3 config
-sudo -u $USER_NAME cp -r /home/$USER_NAME/knowledge-base/pages/system-setup/i3 /home/$USER_NAME/.config
+sudo -u $USER_NAME cp -r /home/$USER_NAME/volt_configs/i3-config/i3 /home/$USER_NAME/.config
 
 # Chrome
 sudo -u $USER_NAME wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 gdebi -n google-chrome-stable_current_amd64.deb
 sudo -u $USER_NAME rm google-chrome-stable_current_amd64.deb
+
+# CLion
+
+cd /home/$USER_NAME/programs
+sudo -u $USER_NAME wget https://download.jetbrains.com/cpp/CLion-2022.1.2.tar.gz
+sudo -u $USER_NAME tar -xf CLion-2022.1.2.tar.gz
+sudo -u $USER_NAME mv clion-2022.1.2 clion
+sudo -u $USER_NAME chmod +x clion/bin/clion.sh
+sudo -u $USER_NAME rm CLion-2022.1.2.tar.gz
+cd /home/$USER_NAME/
 
 apt autoremove
 
